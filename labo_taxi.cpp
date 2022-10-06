5ce1e6e7c966f688b524bb966b3b731a01272ed0
Balkghar <hugo.germano8@gmail.com>
2022-10-06 14:08:48 +0200

    Mise à jour du .cpp

---
+++ labo_taxi.cpp
@@ -0,0 +1,25 @@
+//---------------------------------------------------------
+// Fichier      : labo_taxi.cpp
+// Auteur       : Harun Ouweis & Hugo Germano
+// Date         : 06.10.2022
+
+// But          : Logiciel permettant de calculer le prix d'une course de taxi
+//
+// Remarque     : Second laboratoire de PRG1
+//
+// Modifs       :
+//
+// Compilateur  : 
+//---------------------------------------------------------
+
+#include    <cstdlib>      // EXIT_SUCCESS
+#include    <iostream>     // cout et cin
+#include    <limits>       //utilisé pour avoir les limites numériques (source: https://www.tutorialspoint.com/what-is-the-use-of-cin-ignore-in-cplusplus)
+
+using namespace std;
+
+int main() {
+    
+
+    return EXIT_SUCCESS;
+}
