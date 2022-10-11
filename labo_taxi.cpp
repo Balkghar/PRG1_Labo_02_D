3efe836f229c610c85093dd2b96c4fda0e610a43
Balkghar <hugo.germano8@gmail.com>
2022-10-11 18:52:48 +0200

    Dernière modif avant rendu

--- labo_taxi.cpp
+++ labo_taxi.cpp
@@ -15,12 +15,12 @@
 #include    <cstdlib>      // EXIT_SUCCESS
 #include    <iostream>     // cout et cin
 #include    <limits>       //utilisé pour avoir les limites numériques (source: https://www.tutorialspoint.com/what-is-the-use-of-cin-ignore-in-cplusplus)
-#include    <iomanip>
-#include    <cmath>
+#include    <iomanip>      //pour setw
 
 using namespace std;
 
 int main() {
+
     //déclaration des constantes
     const double    PRISEENCHARGE = 5.00,
                     SUPPPARBAGAGE = 2.60,
@@ -32,11 +32,19 @@ int main() {
                     MAXDISTANCE   = 500,
                     MINNBRBAGAGE  = 0,
                     MAXNBRBAGAGE  = 4,
+                    MINVITESSE    = 50,
+                    MAXVITESSE    = 120,
+                    MINHEURDEPART = 0,
+                    MAXHEURDEPART = 23,
                     W             = 25;
 
 
     //déclaration des variables pour les saisies utilisateur
     int nbrBagage, distance, vitesse, depart;
+
+
+    //prix total par catégories
+    double  totalBagages, prixDeCourse, prixTotal, tempsTrajet;
 
     //fixe la précision à deux nombres après la virgule
     cout << fixed << setprecision(2);
@@ -50,16 +58,13 @@ int main() {
     cout  << left << setw(W) << " - tarif/min (jour) : " << TARIFMINJOUR << endl;
     cout  << left << setw(W) << " - tarif/min (nuit) : " << TARIFMINNUIT << endl;
     cout  << left << setw(W) << " - heures du jour   : " << "[" << DEBUTJOURNEE << "-"<< FINJOURNEE << "]" << endl << endl;
-    //prix total par catégories
-    double  totalBagages, prixDeCourse, prixTotal, tempsTrajet;
-
-
-    //SAISIES UTILISATEURS
+    
+    //SAISIES UTILISATEURS ET VÉRIFICATIONS DE LA SAISIE
     cout << "Votre commande" <<endl
          << "=============="<< endl;
 
     //Bagages
-    cout << "- nbre de bagage    [0 - 4] : ";
+    cout << "- nbre de bagage    ["<< MINNBRBAGAGE <<" - " << MAXNBRBAGAGE <<"] : ";
     cin >> nbrBagage;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     if (MINNBRBAGAGE <= nbrBagage and nbrBagage<= MAXNBRBAGAGE){
@@ -71,7 +76,7 @@ int main() {
     }
 
     //Distance
-    cout << "- distance [km]   [0 - 500] : ";
+    cout << "- distance [km]   [" << MINDISTANCE << " - "<< MAXDISTANCE <<"] : ";
     cin >> distance;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     if (MINDISTANCE > distance or distance >MAXDISTANCE){
@@ -80,10 +85,10 @@ int main() {
     }
 
     //Vitesse
-    cout << "- vitesse [km/h] [50 - 120] : ";
+    cout << "- vitesse [km/h]  [" << MINVITESSE << "- " << MAXVITESSE <<"] : ";
     cin >> vitesse;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    if(50 <= vitesse and vitesse <= 120){
+    if(MINVITESSE <= vitesse and vitesse <= MAXVITESSE){
         tempsTrajet = (double(distance) / vitesse) * 60;
     }
     else {
@@ -92,10 +97,10 @@ int main() {
     }
 
     //Départ
-    cout << "- depart           [0 - 23] : ";
+    cout << "- depart           [" << MINHEURDEPART << " - " << MAXHEURDEPART << "] : ";
     cin >> depart;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    if (0 > depart or depart > 23){
+    if (MINHEURDEPART > depart or depart > MAXHEURDEPART){
         cout << "Votre valeur ne rentre pas dans l'intervalle donné" << endl;
         return (EXIT_FAILURE);
     }
@@ -111,10 +116,9 @@ int main() {
     //Calcul Total
     prixTotal = prixDeCourse + totalBagages + PRISEENCHARGE;
 
-
-    //Afficher
     cout << endl;
 
+    //Affichage du ticket
     cout  << left << setw(W) << "Votre ticket" << endl;
     cout  << left << setw(W) << "============" << endl;
     cout  << left << setw(W) << " - prise en charge   : " << PRISEENCHARGE << endl;
