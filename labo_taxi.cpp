2ba8a1724515386580491a82f0f11cf78d5f5ed4
Balkghar <hugo.germano8@gmail.com>
2022-10-11 11:52:00 +0200

    Affichage

--- labo_taxi.cpp
+++ labo_taxi.cpp
@@ -15,39 +15,117 @@
 #include    <cstdlib>      // EXIT_SUCCESS
 #include    <iostream>     // cout et cin
 #include    <limits>       //utilisé pour avoir les limites numériques (source: https://www.tutorialspoint.com/what-is-the-use-of-cin-ignore-in-cplusplus)
+#include    <iomanip>
+#include    <cmath>
 
 using namespace std;
 
 int main() {
     //déclaration des constantes
-    const double    priseEncharge = 5.00,
-                    suppParBagage = 2.60,
-                    tarifMinJour  = 1.00,
-                    tarifMinNuit  = 1.60;
-    const int       debutJournee  = 8,
-                    finJournee    = 20;
+    const double    PRISEENCHARGE = 5.00,
+                    SUPPPARBAGAGE = 2.60,
+                    TARIFMINJOUR  = 1.00,
+                    TARIFMINNUIT  = 1.60;
+    const int       DEBUTJOURNEE  = 8,
+                    FINJOURNEE    = 20,
+                    MINDISTANCE   = 0,
+                    MAXDISTANCE   = 500,
+                    MINNBRBAGAGE  = 0,
+                    MAXNBRBAGAGE  = 4,
+                    W             = 25;
+
 
     //déclaration des variables pour les saisies utilisateur
     int nbrBagage, distance, vitesse, depart;
 
+    //fixe la précision à deux nombres après la virgule
+    cout << fixed << setprecision(2);
 
-    //déclaration des totaux des différents services
-    double totalBagage, totalCourse;
-
-    //affichage des tarifs à l'utilisateur
-
-
-    //entrée de l'utilisateur
-
-
-    //calcul de l'addition
-
-
-    //Affichage de l'addition
-
-
-    //Message de fin
+    //affichage des tarifs à l'utilisateurs
+    cout  << left << setw(W) << "Bonjour, ce programme permet de calculer le prix d'une course de taxi" << endl;
+    cout  << left << setw(W) << "Voici les conditions" << endl;
+    cout  << left << setw(W) << "====================" << endl;
+    cout  << left << setw(W) << " - prise en charge  : " << PRISEENCHARGE << endl;
+    cout  << left << setw(W) << " - supp par bagage  : " << SUPPPARBAGAGE << endl;
+    cout  << left << setw(W) << " - tarif/min (jour) : " << TARIFMINJOUR << endl;
+    cout  << left << setw(W) << " - tarif/min (nuit) : " << TARIFMINNUIT << endl;
+    cout  << left << setw(W) << " - heures du jour   : " << "[" << DEBUTJOURNEE << "-"<< FINJOURNEE << "]" << endl << endl;
+    //prix total par catégories
+    double  totalBagages, prixDeCourse, prixTotal, tempsTrajet;
 
 
+    //SAISIES UTILISATEURS
+    cout << "Votre commande" <<endl
+         << "=============="<< endl;
+
+    //Bagages
+    cout << "- nbre de bagage    [0 - 4] : ";
+    cin >> nbrBagage;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if (MINNBRBAGAGE <= nbrBagage and nbrBagage<= MAXNBRBAGAGE){
+        totalBagages = nbrBagage * SUPPPARBAGAGE;
+    }
+    else {
+        cout << "Votre valeur ne rentre pas dans l'intervalle donné" << endl;
+        return (EXIT_FAILURE);
+    }
+
+    //Distance
+    cout << "- distance [km]   [0 - 500] : ";
+    cin >> distance;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if (MINDISTANCE > distance or distance >MAXDISTANCE){
+        cout << "Votre valeur ne rentre pas dans l'intervalle donné" << endl;
+        return (EXIT_FAILURE);
+    }
+
+    //Vitesse
+    cout << "- vitesse [km/h] [50 - 120] : ";
+    cin >> vitesse;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if(50 <= vitesse and vitesse <= 120){
+        tempsTrajet = (double(distance) / vitesse) * 60;
+    }
+    else {
+        cout << "Votre valeur ne rentre pas dans l'intervalle donné" << endl;
+        return (EXIT_FAILURE);
+    }
+
+    //Départ
+    cout << "- depart           [0 - 23] : ";
+    cin >> depart;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if (0 > depart or depart > 23){
+        cout << "Votre valeur ne rentre pas dans l'intervalle donné" << endl;
+        return (EXIT_FAILURE);
+    }
+
+    //Test si jour ou nuit et calcul du prix de la course
+    if (DEBUTJOURNEE <= depart and depart <= FINJOURNEE) {
+        prixDeCourse = TARIFMINJOUR * tempsTrajet;
+    }
+    else { 
+        prixDeCourse = TARIFMINNUIT * tempsTrajet;
+    }
+
+    //Calcul Total
+    prixTotal = prixDeCourse + totalBagages + PRISEENCHARGE;
+
+
+    //Afficher
+    cout << endl;
+
+    cout  << left << setw(W) << "Votre ticket" << endl;
+    cout  << left << setw(W) << "============" << endl;
+    cout  << left << setw(W) << " - prise en charge   : " << PRISEENCHARGE << endl;
+    cout  << left << setw(W) << " - supp bagage       : " << totalBagages << endl;
+    cout  << left << setw(W) << " - prix de la course : " << prixDeCourse << endl;
+    cout  << left << setw(W) << "  TOTAL : " << prixTotal << endl << endl;
+
+    //MESSAGE DE FIN
+    cout <<"merci pour votre visite" << endl;
+    cout << "Presser ENTER pour quitter";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    
     return EXIT_SUCCESS;
 }
